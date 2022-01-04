6d37e943e9310619473ddff2108d3678a43647d7
Osama Haider <osamakiani32@gmail.com>
2022-01-04 15:15:05 +0500

    Changes has been updated.

---
+++ TestingLoggerNugetPackege/Program.cs
@@ -0,0 +1,15 @@
+﻿using LoggerNugetPackege;
+using System;
+
+namespace TestingLoggerNugetPackege
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+            logger.Log("this is test dude");
+                Console.WriteLine("Hello World!");
+        }
+    }
+}
